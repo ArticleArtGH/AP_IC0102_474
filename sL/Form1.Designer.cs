8ebd6272a5548269ed85e7e47f91a59841624411
Artyom Chernykh <artemchernykh07@gmail.com>
2020-11-02 08:43:06 +0300

    for myself

---
+++ sL/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace sL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel_main = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel_btn = new System.Windows.Forms.TableLayoutPanel();
+            this.button_add = new System.Windows.Forms.Button();
+            this.panel_Main = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel_main.SuspendLayout();
+            this.tableLayoutPanel_btn.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel_main
+            // 
+            this.tableLayoutPanel_main.ColumnCount = 1;
+            this.tableLayoutPanel_main.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_main.Controls.Add(this.tableLayoutPanel_btn, 0, 1);
+            this.tableLayoutPanel_main.Controls.Add(this.panel_Main, 0, 0);
+            this.tableLayoutPanel_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel_main.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel_main.Name = "tableLayoutPanel_main";
+            this.tableLayoutPanel_main.RowCount = 2;
+            this.tableLayoutPanel_main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.4F));
+            this.tableLayoutPanel_main.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.6F));
+            this.tableLayoutPanel_main.Size = new System.Drawing.Size(324, 375);
+            this.tableLayoutPanel_main.TabIndex = 0;
+            // 
+            // tableLayoutPanel_btn
+            // 
+            this.tableLayoutPanel_btn.ColumnCount = 2;
+            this.tableLayoutPanel_btn.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_btn.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_btn.Controls.Add(this.button_add, 0, 0);
+            this.tableLayoutPanel_btn.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tableLayoutPanel_btn.Location = new System.Drawing.Point(3, 320);
+            this.tableLayoutPanel_btn.Name = "tableLayoutPanel_btn";
+            this.tableLayoutPanel_btn.RowCount = 1;
+            this.tableLayoutPanel_btn.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel_btn.Size = new System.Drawing.Size(318, 52);
+            this.tableLayoutPanel_btn.TabIndex = 0;
+            // 
+            // button_add
+            // 
+            this.button_add.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button_add.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_add.Location = new System.Drawing.Point(3, 3);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(75, 46);
+            this.button_add.TabIndex = 0;
+            this.button_add.Text = "+";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // panel_Main
+            // 
+            this.panel_Main.AutoScroll = true;
+            this.panel_Main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_Main.Location = new System.Drawing.Point(3, 3);
+            this.panel_Main.Name = "panel_Main";
+            this.panel_Main.Size = new System.Drawing.Size(318, 303);
+            this.panel_Main.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 375);
+            this.Controls.Add(this.tableLayoutPanel_main);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel_main.ResumeLayout(false);
+            this.tableLayoutPanel_btn.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_main;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel_btn;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Panel panel_Main;
+    }
+}
+
---
+++ sL/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sL
+{
+
+    public partial class Form1 : Form
+    {
+        class List_Main
+        {
+            public static void LabelDynamic(Panel panel, string text, int width,
+                int height, int left, int top)
+            {
+                Label lbl = new Label();
+                lbl.Text = text;
+                lbl.Width = width;
+                lbl.Height = height;
+                lbl.Left = left;
+                lbl.Top = top;
+                lbl.BackColor = Color.White;
+                lbl.ForeColor = Color.Black;
+                panel.Controls.Add(lbl);
+            }
+
+            public static void ButtonDynamic(Panel panel, string text, int width,
+                int height, int left, int top)
+            {
+                Button btn = new Button();
+                btn.Text = text;
+                btn.Text = text;
+                btn.Width = width;
+                btn.Height = height;
+                btn.Left = left;
+                btn.Top = top;
+                btn.BackColor = Color.Gray;
+                btn.ForeColor = Color.Black;
+                panel.Controls.Add(btn);
+            }
+
+            public static void TextDynamic(Panel panel, string text, int width,
+                int height, int left, int top)
+            {
+                TextBox txt = new TextBox();
+                txt.Text = text;
+                txt.Width = width;
+                txt.Height = height;
+                txt.Left = left;
+                txt.Top = top;
+                txt.BackColor = Color.White;
+                txt.ForeColor = Color.Black;
+                panel.Controls.Add(txt);
+            }
+
+            public static Panel PanelDynamic(/*Panel panel,*/ int left, int top)
+            {
+                Panel pnl = new Panel();
+                pnl.BorderStyle = BorderStyle.FixedSingle;
+                pnl.Width = 300;
+                pnl.Height = 50;
+                pnl.BackColor = Color.White;
+                pnl.ForeColor = Color.Black;
+                pnl.Left = left;
+                pnl.Top = top;
+                //pnl.Controls.Add(panel);
+                return pnl;
+            }
+
+            //public static void ClassDynamic(Panel panel, string titleLbl,
+            //    string changeBtn, string txtBtn, string delBtn, int left,
+            //    int top)
+            public static void ClassDynamic(Panel panel, int left,
+                int top)
+            {
+                Panel pnl = PanelDynamic(left, top);
+                LabelDynamic(pnl, "Title", 50, 30, 10, 10);
+                ButtonDynamic(pnl, "Change", 60, 30, 60, 10);
+                TextDynamic(pnl, "Comments", 100, 30, 120, 10);
+                ButtonDynamic(pnl, "Delete", 50, 30, 220, 10);
+                panel.Controls.Add(pnl);
+            }
+        }
+
+        //Label lbl = new Label();
+        //Button btn = new Button();
+        //TextBox txt = new TextBox();
+        //Panel pnl = new Panel();
+        int left = 0, top = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            List_Main.ClassDynamic(this.panel_Main, left, top);
+            top += 60;
+
+            ////gb.FlatStyle = FlatStyle.Flat;
+            //pnl.Text = "Anime";
+            //pnl.Location = new Point(10, 10);
+
+            //lbl.Text = "Title1";
+            //lbl.Location = new Point(10, 10);
+            //pnl.Controls.Add(lbl);
+
+            //btn.Text = "Change";
+            //btn.Location = new Point(50, 10);
+            //pnl.Controls.Add(btn);
+
+            //txt.Size = new Size(30, 25);
+            //txt.Location = new Point(100, 10);
+            //pnl.Controls.Add(txt);
+
+            //btn.Text = "Delete";
+            //btn.Location = new Point(130,10 );
+            //pnl.Controls.Add(btn);
+
+            //this.tableLayoutPanel_main.Controls.Add(pnl);
+
+
+        }
+    }
+}
